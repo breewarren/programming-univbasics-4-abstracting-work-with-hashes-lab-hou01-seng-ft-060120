ae5bc0a79d4d83a1a937286a957fee633e459a6c
Bree Warren <ambreea.warren@gmail.com>
2020-05-16 15:23:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,8 @@
 def my_hash_creator(key, value)
 {key: "value"
 }
-my_hash_creator
 end
+my_hash_creator
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
